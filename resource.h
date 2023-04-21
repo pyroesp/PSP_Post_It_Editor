0e32f32cadb5e03ef71b4777bcc844fdd5ef624a
pyroesp <nicolaslopezesp@gmail.com>
2023-04-22 00:40:18 +0200

    changed image folder to relative path

--- resource.h
+++ resource.h
@@ -1,15 +1,15 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
-#define RES_IMAGE_PATH "ms0:/PSP/GAME/PSP Post It - Editor/images/"
-#define RES_MAIN "main.png"
-#define RES_VIEW "view.png"
-#define RES_EDIT "edit.png"
-#define RES_ADD "add.png"
-#define RES_REMOVE "remove.png"
-#define RES_SAVE "save.png"
-#define RES_EXIT "exit.png"
-#define RES_CREDIT "credit.png"
+#define RES_IMAGE_PATH	"./images/"
+#define RES_MAIN		"main.png"
+#define RES_VIEW 		"view.png"
+#define RES_EDIT 		"edit.png"
+#define RES_ADD 		"add.png"
+#define RES_REMOVE 		"remove.png"
+#define RES_SAVE 		"save.png"
+#define RES_CREDIT 		"credit.png"
+#define RES_EXIT 		"exit.png"
 
 
 // use enum for quick constants
@@ -24,7 +24,9 @@ enum MainMenu{
 };
 
 
-char main_menu[7][10] = {
+#define RES_MENU_OPTIONS 7
+
+char main_menu[RES_MENU_OPTIONS][10] = {
 	"View",
 	"Edit",
 	"Add",
